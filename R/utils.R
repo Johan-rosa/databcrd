0123add70d45bdb4e68ab65f7b1f9b7505972f4e
Johan Rosa <johan.rosaperez@gmail.com>
2023-04-14 21:40:25 -0400

    Add rescale function

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,28 @@ date_label <- function(date = Sys.Date()) {
     lubridate::year(date)
   )
 }
+
+#' Rescale a vector
+#'
+#' With this function you can change the scale of a numeric vector providing the
+#' new minimum and maximum values
+#'
+#' @param x a numeric vector
+#' @param new_min numeric scalar with the minimum value desired
+#' @param new_max numeric scalar with the maximum value desired
+#'
+#' @return a numeric vector
+#' @export
+#'
+#' @examples
+#' rescale(1:10)
+rescale <- function(x, new_min = 0, new_max = 1) {
+  checkmate::assert_numeric(x)
+  checkmate::assert_numeric(new_min)
+  checkmate::assert_numeric(new_max)
+
+  old_range <-  (max(x) - min(x))
+  new_range <-  (new_max - (new_min))
+  new_value <-  (((x - min(x)) * new_range) / old_range) + (new_min)
+  return(new_value)
+}
