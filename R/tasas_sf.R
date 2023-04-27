d81597ec20ac0f8040d65befe03877f198f054be
Johan Rosa <johan.rosaperez@gmail.com>
2023-04-27 14:39:17 -0400

    Tasas pasivas functional approach

--- R/tasas_sf.R
+++ R/tasas_sf.R
@@ -1,5 +1,5 @@
 get_tasas_pasivas <- function() {
-  names_files <- c(
+  file_names <- c(
     "Tasas 1991-2007",
     "Tasas 2008-2012",
     "Tasas 2013-2016",
@@ -28,108 +28,69 @@ get_tasas_pasivas <- function() {
      "documents/tbm_pasivad.xlsx")
    )
 
-  names(urls) <- names_files
+  names(urls) <- file_names
 
   temp_files <- base::sapply(
     c(".xls", ".xls", ".xlsx", ".xlsx"),
     \(x) tempfile(fileext = x)
-  ) |> stats::setNames(names_files)
+  ) |>
+    stats::setNames(file_names)
 
-  purrr::walk2(urls, temp_files, utils::download.file, quiet = TRUE)
-
-  reading_arguments = pmap(
-    purrr::
-      \(file, sheet, range, skip){}
+  purrr::walk2(
+    urls, temp_files,
+    utils::download.file, mode = "wb", quiet = TRUE
   )
 
-  # Reading the files
-  pasivas_9107 <- readxl::read_excel(
-    pasivas_9107_path, sheet = "Pasivas",
-    col_names = FALSE, range = "B157:M266"
+  params <- list(
+    list(range = "A157:M266"),
+    list(range = "A14:O88"),
+    list(range = "A11:O67"),
+    list(skip = 10)
+  ) |>
+    purrr::map2(
+      temp_files,
+      \(x, y) append(x, list(path = y, col_names = FALSE))
+    )
+
+  tasas_list <- purrr::map(
+    seq_along(file_names),
+    \(index) do.call(readxl::read_excel, params[[index]])
+    ) |>
+    stats::setNames(file_names)
+
+  prepare_data <- function(df, names, start_date) {
+    df |>
+      stats::setNames(names) |>
+      dplyr::filter(stringr::str_detect(mes, "^[A-Z]")) |>
+      dplyr::mutate(
+        fecha = seq(as.Date(start_date), by = "month", length.out = dplyr::n()),
+        year = lubridate::year(fecha),
+        mes = lubridate::month(fecha),
+        mes = crear_mes(mes, "number_to_text")
+      ) |>
+      dplyr::select(fecha, year, mes, dplyr::everything())
+  }
+
+  df_names <- list(
+    c("mes", "tp_30d", "tp_60d", "tp_90d", "tp_180d", "tp_360", "tp_m360",
+      "tp_ps", "tp_pp", "tp_dep_ahorros", "tp_preferencial", "tp_general",
+      "tp_interbancarios"),
+    c("mes", "tp_30d", "tp_60d", "tp_90d", "tp_180d", "tp_360d",
+      "tp_2a", "tp_5a", "tp_m5a", "tp_pp", "tp_ps", "tp_dep_ahorros",
+      "tp_general", "tp_preferencial", "tp_interbancarios")
   )
 
-  pasivas_0812 <- readxl::read_excel(
-    pasivas2_0812_path, sheet = "Pasivas",
-    col_names = FALSE, range = "A14:O88")
+  df_names <- list(df_names[[1]], df_names[[2]], df_names[[2]], df_names[[2]])
 
-  pasivas_1316 <- readxl::read_excel(
-    pasivas3_1316_path, sheet = "Pasivas",
-    col_names = FALSE, range = "A11:O67")
+  df_cleaning_params <- dplyr::lst(
+    df = tasas_list,
+    names = df_names,
+    start_date = c("2000/01/01", "2008/01/01", "2013/01/01", "2017/01/01")
+  )
 
-  pasivas_17 <- readxl::read_excel(
-    pasivas4_17_path, sheet = "Pasivas",
-    col_names = FALSE, skip = 10)
-
-  # Wrangling 1991-2007 rates --- ---
-  pasivas_9107 <- pasivas_9107 |>
-    stats::setNames(
-      c("tp_30d", "tp_60d", "tp_90d", "tp_180d", "tp_360", "tp_m360",
-        "tp_ps", "tp_pp", "tp_dep_ahorros", "tp_preferencial", "tp_general",
-        "tp_interbancarios")
-    ) |>
-    tidyr::drop_na() |>
-    dplyr::mutate(
-      fecha = seq(as.Date("2000/01/01"), by = "month", length.out = dplyr::n()),
-      year = lubridate::year(fecha),
-      mes = lubridate::month(fecha),
-      mes = crear_mes(mes, "number_to_text")
-    ) |>
-    dplyr::select(fecha, year, mes, dplyr::everything())
-
-  # Wrangling 2008-2012 rates --- ---
-  pasivas_0812 <- pasivas_0812 |>
-    stats::setNames(
-      c("mes", "tp_30d", "tp_60d", "tp_90d", "tp_180d", "tp_360d",
-        "tp_2a", "tp_5a", "tp_m5a", "tp_pp", "tp_ps", "tp_dep_ahorros",
-        "tp_general", "tp_preferencial", "tp_interbancarios")
-    ) |>
-    dplyr::filter(stringr::str_detect(mes, "^[A-Z]")) |>
-    dplyr::mutate(
-      fecha = seq(as.Date("2008/01/01"), by = "month", length.out = dplyr::n()),
-      year = lubridate::year(fecha),
-      mes = lubridate::month(fecha),
-      mes = crear_mes(mes, "number_to_text")
-    ) |>
-    dplyr::select(fecha, year, mes, dplyr::everything())
-
-  # Wrangling 2013-2016 rates --- ---
-
-  pasivas_1316 <- pasivas_1316 |>
-    setNames(
-      c("mes", "tp_30d", "tp_60d", "tp_90d",  "tp_180d", "tp_360d",
-        "tp_2a", "tp_5a", "tp_m5a", "tp_pp", "tp_ps", "tp_dep_ahorros",
-        "tp_general", "tp_preferencial", "tp_interbancarios")
-    ) |>
-    dplyr::filter(stringr::str_detect(mes, "^[A-Z]")) |>
-    dplyr::mutate(
-      fecha = seq(as.Date("2013/01/01"), by = "month", length.out = dplyr::n()),
-      year = lubridate::year(fecha),
-      mes = lubridate::month(fecha),
-      mes = crear_mes(mes, "number_to_text")
-    ) |>
-    dplyr::select(fecha, year, mes, everything())
-
-  # Wrangling 2017-present rates --- ---
-
-  pasivas_17 <- pasivas_17 |>
-    setNames(
-      c("mes", "tp_30d", "tp_60d", "tp_90d",  "tp_180d", "tp_360d",
-        "tp_2a", "tp_5a", "tp_m5a", "tp_pp", "tp_ps", "tp_dep_ahorros",
-        "tp_general", "tp_preferencial", "tp_interbancarios")
-    ) |>
-    dplyr::filter(stringr::str_detect(mes, "^[A-Z]")) |>
-    dplyr::filter(!is.na(.data[["tp_30d"]])) |>
-    dplyr::mutate(
-      fecha = seq(as.Date("2017/01/01"), by = "month", length.out = dplyr::n()),
-      year = lubridate::year(fecha),
-      mes = lubridate::month(fecha),
-      mes = crear_mes(mes, "number_to_text")
-    ) |>
-    dplyr::select(fecha, year, mes, everything())
-
-  dplyr::bind_rows(
-    pasivas_9107,
-    pasivas_0812,
-    pasivas_1316,
-    pasivas_17)
+  purrr::pmap(
+    df_cleaning_params,
+    prepare_data
+  ) |>
+    purrr::list_rbind()
 }
