a0acfc111897460b19af8b3afea7116345c8152d
Johan Rosa <johan.rosaperez@gmail.com>
2023-05-09 19:05:45 -0400

    Indicadores monetarios y financieros del BCRD (#10)
    * Indicadores monetarios del BCRD
    
    * Ungroup indicadores BCRD
    
    * To pass linters

--- R/global.R
+++ R/global.R
@@ -1,3 +1,5 @@
 utils::globalVariables(
-  c("fecha", "mes", "year", "ipc_ayb", "ta_90d", "tp_30d", "ano", "dia",
-    "trimestre"))
+  c("fecha", "mes", "year", "ipc_ayb", "ano", "dia", "trimestre",
+    "ta_90d", "tp_30d",
+    "descripcion", "short_names", "categoria", "nivel", "original_names",
+    "labels", "direct_parent", "indicadores_bcrd_details"))
