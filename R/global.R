366390dddbf895b99fac95d7090e32cf620e12b8
Johan Rosa <johan.rosaperez@gmail.com>
2023-05-11 20:17:07 -0400

    PIB por método del gasto (#12)
    * get_pib_gasto() logic
    
    * get_pib_gasto() tests and documentation
    
    ---------
    
    Co-authored-by: Johan Rosa <j.rosa@bancentral.gov.do>

--- R/global.R
+++ R/global.R
@@ -1,5 +1,5 @@
 utils::globalVariables(
   c("fecha", "mes", "year", "ipc_ayb", "ano", "dia", "trimestre",
-    "ta_90d", "tp_30d",
+    "ta_90d", "tp_30d", "partida",
     "descripcion", "short_names", "categoria", "nivel", "original_names",
     "labels", "direct_parent", "indicadores_bcrd_details"))
