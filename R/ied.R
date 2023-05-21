d3199a35a3ad2ee3d85722321d2e1f2d6a5b3737
juansqw <juansqw@gmail.com>
2023-05-21 17:16:28 -0600

    Funciones para descargar IED

---
+++ R/ied.R
@@ -0,0 +1,129 @@
+#' Foreing direct investment to the Dominican Republic
+#'
+#' This function returns the  Foreing direct investment to the
+#' Dominican Republicthe based on the specified frequency.
+#'
+#' @param modalidad A character string. Options are "pais_origen" or
+#' "sector_destino".
+#' @param frecuencia A character string that specifies the frequency of the
+#' exchange rates to be downloaded. Valid options are "trimestral",  or "anual".
+#'
+#' @return A data frame
+#'
+#' @export
+#'
+#' @examples
+#'get_tc_eif("mensual")
+#'get_tc_eif("anual")
+
+get_ied <- function(modalidad = "pais_origen",
+                    frecuencia = "trimestral") {
+  checkmate::assert_choice(modalidad, c("pais_origen", "sector_destino"))
+  checkmate::assert_choice(frecuencia, c("trimestral", "anual"))
+
+  if (modalidad == "pais_origen") return(get_ied_pais(frecuencia))
+  if (modalidad == "sector_destino") return(get_ied_sector(frecuencia))
+
+}
+
+#' Get Foreign Direct Investment by country of origin
+get_ied_pais <- function(frecuencia = "trimestral") {
+  checkmate::assert_choice(frecuencia, c("trimestral", "anual"))
+
+  file_url <- paste0("https://cdn.bancentral.gov.do/documents/",
+                     "estadisticas/sector-externo/",
+                     "documents/inversion_ext_pais_6.xls")
+  file_path <- base::tempfile(pattern = "", fileext = ".xls")
+  utils::download.file(file_url, file_path, mode = "wb", quiet = TRUE)
+
+  suppressMessages(
+    ied_data <- readxl::read_excel(
+      path = file_path,
+      skip = 7,
+      n_max = 20
+    )
+  )
+
+  quarters <- seq(
+    as.Date("2010-01-01"),
+    by = "quarter",
+    length.out = ncol(ied_data) - 1
+  )
+
+  ied_data <- ied_data |>
+    stats::setNames(c("partida", quarters)) |>
+    tidyr::pivot_longer(
+      cols = -partida,
+      names_to = "trimestre",
+      values_to = "monto") |>
+    dplyr::mutate(
+      trimestre = as.numeric(trimestre),
+      fecha = lubridate::as_date(trimestre),
+      year = lubridate::year(fecha),
+      trimestre = lubridate::quarter(fecha)
+    )
+
+  if (frecuencia == "anual") {
+    ied <- suppressMessages(
+      ied_data |>
+        dplyr::select(-c(trimestre, fecha)) |>
+        dplyr::group_by(partida, year) |>
+        dplyr::summarise(monto = sum(monto))
+    )
+  } else if (frecuencia == "trimestral") {
+    ied <- ied_data
+  }
+
+  ied
+}
+
+#' Get Foreign Direct Investment by sector
+get_ied_sector <- function(frecuencia = "trimestral") {
+  checkmate::assert_choice(frecuencia, c("trimestral", "anual"))
+
+  file_url <- paste0("https://cdn.bancentral.gov.do/documents/",
+                     "estadisticas/sector-externo/",
+                     "documents/inversion_ext_sector_6.xls")
+  file_path <- base::tempfile(pattern = "", fileext = ".xls")
+  utils::download.file(file_url, file_path, mode = "wb", quiet = TRUE)
+
+  suppressMessages(
+    ied_data <- readxl::read_excel(
+      path = file_path,
+      skip = 7,
+      n_max = 10
+    )
+  )
+
+  quarters <- seq(
+    as.Date("2010-01-01"),
+    by = "quarter",
+    length.out = ncol(ied_data) - 1
+  )
+
+  ied_data <- ied_data |>
+    stats::setNames(c("partida", quarters)) |>
+    tidyr::pivot_longer(
+      cols = -partida,
+      names_to = "trimestre",
+      values_to = "monto") |>
+    dplyr::mutate(
+      trimestre = as.numeric(trimestre),
+      fecha = lubridate::as_date(trimestre),
+      year = lubridate::year(fecha),
+      trimestre = lubridate::quarter(fecha)
+    )
+
+  if (frecuencia == "anual") {
+    ied <- suppressMessages(
+      ied_data |>
+        dplyr::select(-c(trimestre, fecha)) |>
+        dplyr::group_by(partida, year) |>
+        dplyr::summarise(monto = sum(monto))
+    )
+  } else if (frecuencia == "trimestral") {
+    ied <- ied_data
+  }
+
+  ied
+}
