cae63285101e5355048ce2b8f8c32e8d1c59f24a
Johan Rosa <johan.rosaperez@gmail.com>
2025-02-06 14:16:58 -0400

    Adjust get_embi
    It was failing due to flying values in the file. It doesn't use a fixed number of columns anymore

--- R/embi.R
+++ R/embi.R
@@ -14,52 +14,54 @@
 #' get_embi("mensual")
 #' get_embi("trimestral")
 #' get_embi("anual")
-get_embi <- function(periodicidad) {
+get_embi <- function(periodicidad = "mensual") {
   checkmate::assert_choice(
     periodicidad,
     choices = c("diario", "semanal", "mensual", "trimestral", "anual")
   )
-  
-  per_table <- tibble::tibble(x = c("diario", "semanal", 
-                                    "mensual", "trimestral", "anual"),
-                              y = c("day", "week", "month", "quarter", "year"))
-  
-  periodicidad <- per_table |> 
-    dplyr::filter(x == periodicidad) |> 
-    dplyr::pull(y)
+
+  periodicidad <- switch(
+    periodicidad,
+    diario = "day",
+    semanal = "week",
+    mensual = "month",
+    trimestral = "quarter",
+    anual = "year",
+    stop("Frecuencia no definida")
+  )
 
   url_descarga <- paste0(
     "https://cdn.bancentral.gov.do/documents/",
     "entorno-internacional/documents/",
     "Serie_Historica_Spread_del_EMBI.xlsx")
-  
+
   file_path <- tempfile(pattern = "", fileext = ".xlsx")
-  
+
   utils::download.file(url_descarga, file_path, mode = "wb", quiet = TRUE)
-  
+
   suppressWarnings(
     suppressMessages(
       embi <- readxl::read_excel(
         file_path,
         sheet = 1,
         col_names = TRUE,
-        col_types = c("text", rep("numeric", 20)),
+        col_types = c("text"),
         skip = 1,
-        na = "N/A") |> 
+        na = "N/A"
+      ) |>
+        dplyr::select(1:`RD-LATINO`) |>
         dplyr::mutate(
           Fecha = dplyr::case_when(
             stringr::str_detect(Fecha, "^\\d+$") ~ janitor::excel_numeric_to_date(as.numeric(Fecha)),
             TRUE ~ lubridate::dmy(Fecha)
-          )
+          ),
+          dplyr::across(-Fecha, as.numeric)
         )
     )
   )
-  
-  
-  embi <- embi |> 
-    dplyr::mutate(Fecha = lubridate::floor_date(Fecha, periodicidad)) |> 
-    dplyr::group_by(Fecha) |> 
-    dplyr::summarise(dplyr::across(tidyselect::everything(),
-                                   .f = \(x) mean(x, na.rm = TRUE)))
-  
+
+  embi |>
+    dplyr::mutate(Fecha = lubridate::floor_date(Fecha, periodicidad)) |>
+    dplyr::group_by(Fecha) |>
+    dplyr::summarise(dplyr::across(tidyselect::everything(), .f = \(x) mean(x, na.rm = TRUE)))
 }
