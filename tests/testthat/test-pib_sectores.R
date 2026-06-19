7dbde23bac9027c1bca661a62c6c0e46c1390917
Johan-rosa <j.rosa@bancentral.gov.do>
2026-06-19 17:11:00 -0400

    feat: adjust test to new source

--- tests/testthat/test-pib_sectores.R
+++ tests/testthat/test-pib_sectores.R
@@ -1,18 +1,18 @@
 
 # PIB nominal series homogenea 1991 ----------------------------------------
-pib_gasto_nominal_homogenea <- get_pib_sectores(
+pib_sectores_homogenea <- get_pib_sectores(
   modalidad = "nominal", homogenea_91 = TRUE)
 
 test_that("Series starts at january 1991", {
-  expect_equal(min(pib_gasto_nominal_homogenea$fecha), as.Date("1991-01-01"))
+  expect_equal(min(pib_sectores_homogenea$fecha), as.Date("1991-01-01"))
 })
 
 test_that("Series doesn't ends in the future", {
-  expect_true(max(pib_gasto_nominal_homogenea$fecha) <= lubridate::today())
+  expect_true(max(pib_sectores_homogenea$fecha) <= lubridate::today())
 })
 
 test_that("All series have the same length", {
-  n_by_partida <- pib_gasto_nominal_homogenea |>
+  n_by_partida <- pib_sectores_homogenea |>
     dplyr::count(sector)
 
   expect_true(max(n_by_partida$n) == min(max(n_by_partida$n)))
@@ -20,25 +20,25 @@ test_that("All series have the same length", {
 
 test_that("Correct column names", {
   expect_equal(
-    names(pib_gasto_nominal_homogenea),
+    names(pib_sectores_homogenea),
     c("sector", "fecha", "year", "trimestre", "pib_nominal", "ponderacion")
   )
 })
 
 
-# PIB nominal series 2007 --------------------------------------------------
-pib_gasto_nominal_2007 <- get_pib_sectores(modalidad = "nominal")
+# PIB nominal series 2018--------------------------------------------------
+pib_sectores_nominal_2018 <- get_pib_sectores(modalidad = "nominal")
 
 test_that("Series starts at january 1991", {
-  expect_equal(min(pib_gasto_nominal_2007$fecha), as.Date("2007-01-01"))
+  expect_equal(min(pib_sectores_nominal_2018$fecha), as.Date("2018-01-01"))
 })
 
 test_that("Series doesn't ends in the future", {
-  expect_true(max(pib_gasto_nominal_2007$fecha) <= lubridate::today())
+  expect_true(max(pib_sectores_nominal_2018$fecha) <= lubridate::today())
 })
 
 test_that("All series have the same length", {
-  n_by_partida <- pib_gasto_nominal_2007 |>
+  n_by_partida <- pib_sectores_nominal_2018 |>
     dplyr::count(sector)
 
   expect_true(max(n_by_partida$n) == min(max(n_by_partida$n)))
@@ -46,43 +46,43 @@ test_that("All series have the same length", {
 
 test_that("Correct column names", {
   expect_equal(
-    names(pib_gasto_nominal_2007),
+    names(pib_sectores_nominal_2018),
     c("sector", "fecha", "year", "trimestre", "pib_nominal", "ponderacion")
   )
 })
 
 test_that("PIB for december 2007 is Ok", {
-  pib_2007 <- pib_gasto_nominal_2007 |>
-    dplyr::filter(sector == "Producto Interno Bruto", fecha == "2007-10-01") |>
+  pib_2018 <- pib_sectores_nominal_2018 |>
+    dplyr::filter(sector == "Producto Interno Bruto", fecha == "2018-10-01") |>
     dplyr::pull(pib_nominal)
 
-  expect_equal(pib_2007, 391153.303668817)
+  expect_equal(pib_2018, 1122176.37792465)
 })
 
 test_that("PIB for december 2016 is Ok", {
-  pib_2007 <- pib_gasto_nominal_2007 |>
-    dplyr::filter(sector == "Producto Interno Bruto", fecha == "2016-10-01") |>
+  pib_2020 <- pib_gasto_nominal_2018 |>
+    dplyr::filter(sector == "Producto Interno Bruto", fecha == "2020-10-01") |>
     dplyr::pull(pib_nominal)
 
-  expect_equal(pib_2007, 926379.831619056)
+  expect_equal(pib_2020, 1239354.12086464)
 })
 
 
 # PIB nominal series 2007 acumulada -------------------------------------------
-pib_gasto_nominal_2007_acumulado <- get_pib_sectores( # nolint
+pib_sectores_nominal_2018_acumulado <- get_pib_sectores( # nolint
   modalidad = "nominal", acumulado = TRUE)
 
-test_that("Series starts at january 1991", {
+test_that("Series starts at january 2018", {
   expect_equal(
-    min(pib_gasto_nominal_2007_acumulado$fecha), as.Date("2007-01-01"))
+    min(pib_sectores_nominal_2018_acumulado$fecha), as.Date("2018-01-01"))
 })
 
 test_that("Series doesn't ends in the future", {
-  expect_true(max(pib_gasto_nominal_2007_acumulado$fecha) <= lubridate::today())
+  expect_true(max(pib_sectores_nominal_2018_acumulado$fecha) <= lubridate::today())
 })
 
 test_that("All series have the same length", {
-  n_by_partida <- pib_gasto_nominal_2007_acumulado |>
+  n_by_partida <- pib_sectores_nominal_2018_acumulado |>
     dplyr::count(sector)
 
   expect_true(max(n_by_partida$n) == min(max(n_by_partida$n)))
@@ -90,17 +90,17 @@ test_that("All series have the same length", {
 
 test_that("Correct column names", {
   expect_equal(
-    names(pib_gasto_nominal_2007_acumulado),
+    names(pib_sectores_nominal_2018_acumulado),
     c("sector", "fecha", "year", "trimestre", "pib_nominal", "ponderacion")
   )
 })
 
-test_that("PIB for december 2007 is Ok", {
-  pib_2007 <- pib_gasto_nominal_2007_acumulado |>
-    dplyr::filter(sector == "Producto Interno Bruto", fecha == "2007-10-01") |>
+test_that("PIB for december 2018 is Ok", {
+  pib_2018 <- pib_sectores_nominal_2018_acumulado |>
+    dplyr::filter(sector == "Producto Interno Bruto", fecha == "2018-10-01") |>
     dplyr::pull(pib_nominal)
 
-  expect_equal(pib_2007, 1458416.51805624)
+  expect_equal(pib_2018, 4208088.58876332)
 })
 
 test_that("PIB for december 2016 is Ok", {
