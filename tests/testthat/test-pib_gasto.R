868e831f1a0e502a080f1a51dc6ce769e9d7573f
Johan Rosa <johan.rosaperez@gmail.com>
2026-06-22 11:05:01 -0400

    feat: pib gasto con nueva base 2018 (#47)
    * feat: pib gasto con nueva base 2018
    
    ---------
    
    Co-authored-by: Johan-rosa <j.rosa@bancentral.gov.do>

--- tests/testthat/test-pib_gasto.R
+++ tests/testthat/test-pib_gasto.R
@@ -27,18 +27,18 @@ test_that("Correct column names", {
 
 
 # PIB nominal series 2007 --------------------------------------------------
-pib_gasto_nominal_2007 <- get_pib_gasto(modalidad = "nominal")
+pib_gasto_nominal_2018 <- get_pib_gasto(modalidad = "nominal")
 
-test_that("Series starts at january 1991", {
-  expect_equal(min(pib_gasto_nominal_2007$fecha), as.Date("2007-01-01"))
+test_that("Series starts at january 2018", {
+  expect_equal(min(pib_gasto_nominal_2018$fecha), as.Date("2018-01-01"))
 })
 
 test_that("Series doesn't ends in the future", {
-  expect_true(max(pib_gasto_nominal_2007$fecha) <= lubridate::today())
+  expect_true(max(pib_gasto_nominal_2018$fecha) <= lubridate::today())
 })
 
 test_that("All series have the same length", {
-  n_by_partida <- pib_gasto_nominal_2007 |>
+  n_by_partida <- pib_gasto_nominal_2018 |>
     dplyr::count(partida)
 
   expect_true(max(n_by_partida$n) == min(max(n_by_partida$n)))
@@ -46,43 +46,43 @@ test_that("All series have the same length", {
 
 test_that("Correct column names", {
   expect_equal(
-    names(pib_gasto_nominal_2007),
+    names(pib_gasto_nominal_2018),
     c("partida", "fecha", "year", "trimestre", "pib_nominal", "ponderacion")
   )
 })
 
-test_that("PIB for december 2007 is Ok", {
-  pib_2007 <- pib_gasto_nominal_2007 |>
-    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2007-10-01") |>
+test_that("PIB for december 2018 is Ok", {
+  pib_2018 <- pib_gasto_nominal_2018 |>
+    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2018-10-01") |>
     dplyr::pull(pib_nominal)
 
-  expect_equal(pib_2007, 391153.303668817)
+  expect_equal(pib_2018, 1122176.37792465)
 })
 
-test_that("PIB for december 2016 is Ok", {
-  pib_2007 <- pib_gasto_nominal_2007 |>
-    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2016-10-01") |>
+test_that("PIB for december 2020 is Ok", {
+  pib_2020 <- pib_gasto_nominal_2018 |>
+    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2020-10-01") |>
     dplyr::pull(pib_nominal)
 
-  expect_equal(pib_2007, 926379.831619056)
+  expect_equal(pib_2020, 1239354.12086464)
 })
 
 
 # PIB nominal series 2007 acumulada -------------------------------------------
-pib_gasto_nominal_2007_acumulado <- get_pib_gasto( # nolint
+pib_gasto_nominal_2018_acumulado <- get_pib_gasto( # nolint
   modalidad = "nominal", acumulado = TRUE)
 
-test_that("Series starts at january 1991", {
+test_that("Series starts at january 2018", {
   expect_equal(
-    min(pib_gasto_nominal_2007_acumulado$fecha), as.Date("2007-01-01"))
+    min(pib_gasto_nominal_2018_acumulado$fecha), as.Date("2018-01-01"))
 })
 
 test_that("Series doesn't ends in the future", {
-  expect_true(max(pib_gasto_nominal_2007_acumulado$fecha) <= lubridate::today())
+  expect_true(max(pib_gasto_nominal_2018_acumulado$fecha) <= lubridate::today())
 })
 
 test_that("All series have the same length", {
-  n_by_partida <- pib_gasto_nominal_2007_acumulado |>
+  n_by_partida <- pib_gasto_nominal_2018_acumulado |>
     dplyr::count(partida)
 
   expect_true(max(n_by_partida$n) == min(max(n_by_partida$n)))
@@ -90,40 +90,40 @@ test_that("All series have the same length", {
 
 test_that("Correct column names", {
   expect_equal(
-    names(pib_gasto_nominal_2007_acumulado),
+    names(pib_gasto_nominal_2018_acumulado),
     c("partida", "fecha", "year", "trimestre", "pib_nominal", "ponderacion")
   )
 })
 
-test_that("PIB for december 2007 is Ok", {
-  pib_2007 <- pib_gasto_nominal_2007_acumulado |>
-    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2007-10-01") |>
+test_that("PIB for december 2018 is Ok", {
+  pib_2018 <- pib_gasto_nominal_2018_acumulado |>
+    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2018-10-01") |>
     dplyr::pull(pib_nominal)
 
-  expect_equal(pib_2007, 1458416.51805624)
+  expect_equal(pib_2018, 4208088.58876332)
 })
 
-test_that("PIB for december 2016 is Ok", {
-  pib_2016 <- pib_gasto_nominal_2007_acumulado |>
-    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2016-10-01")
+test_that("PIB for december 2020 is Ok", {
+  pib_2020 <- pib_gasto_nominal_2018_acumulado |>
+    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2020-10-01")
 
-  expect_equal(pib_2016$pib_nominal, 3487292.51270275)
-  expect_equal(pib_2016$ponderacion, 100)
+  expect_equal(pib_2020$pib_nominal, 4439813.97652647)
+  expect_equal(pib_2020$ponderacion, 100)
 })
 
 # PIB real series 2007 --------------------------------------------------
-pib_gasto_real_2007 <- get_pib_gasto(modalidad = "real")
+pib_gasto_real_2018 <- get_pib_gasto(modalidad = "real")
 
-test_that("Series starts at january 1991", {
-  expect_equal(min(pib_gasto_real_2007$fecha), as.Date("2007-01-01"))
+test_that("Series starts at january 2018", {
+  expect_equal(min(pib_gasto_real_2018$fecha), as.Date("2018-01-01"))
 })
 
 test_that("Series doesn't ends in the future", {
-  expect_true(max(pib_gasto_real_2007$fecha) <= lubridate::today())
+  expect_true(max(pib_gasto_real_2018$fecha) <= lubridate::today())
 })
 
 test_that("All series have the same length", {
-  n_by_partida <- pib_gasto_real_2007 |>
+  n_by_partida <- pib_gasto_real_2018 |>
     dplyr::count(partida)
 
   expect_true(max(n_by_partida$n) == min(max(n_by_partida$n)))
@@ -131,25 +131,25 @@ test_that("All series have the same length", {
 
 test_that("Correct column names", {
   expect_equal(
-    names(pib_gasto_real_2007),
+    names(pib_gasto_real_2018),
     c("partida", "fecha", "year", "trimestre", "indice",
       "crecimiento_interanual", "incidencia")
   )
 })
 
-test_that("PIB for december 2007 is Ok", {
-  pib_2007 <- pib_gasto_real_2007 |>
-    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2007-10-01") |>
+test_that("PIB for december 2018 is Ok", {
+  pib_2018 <- pib_gasto_real_2018 |>
+    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2018-10-01") |>
     dplyr::pull(indice)
 
-  expect_equal(pib_2007, 103.525353112715)
+  expect_equal(pib_2018, 103.97372424462)
 })
 
 test_that("PIB for december 2016 is Ok", {
-  pib_2016 <- pib_gasto_real_2007 |>
-    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2016-10-01")
+  pib_2020 <- pib_gasto_real_2018 |>
+    dplyr::filter(partida == "Producto Interno Bruto", fecha == "2020-10-01")
 
-  expect_equal(pib_2016$indice, 157.009259740035)
-  expect_equal(pib_2016$crecimiento_interanual, 5.39533315364895)
-  expect_equal(pib_2016$incidencia, 5.39533315364895)
+  expect_equal(pib_2020$indice, 102.819041568166)
+  expect_equal(pib_2020$crecimiento_interanual, -5.42031754140824)
+  expect_equal(pib_2020$incidencia, -5.42031754140824)
 })
