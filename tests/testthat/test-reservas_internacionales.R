9c84e3d72449f0971ec32b22c233aa450978f97c
Johan Rosa <johan.rosaperez@gmail.com>
2026-06-22 12:21:23 -0400

    Feat: función para descargar reservas internacionales (#48)
    * feat: reservas internacionales.
    
    * test: test resevas internacionales
    
    ---------
    
    Co-authored-by: Johan-rosa <j.rosa@bancentral.gov.do>

---
+++ tests/testthat/test-reservas_internacionales.R
@@ -0,0 +1,39 @@
+# tests/testthat/test-parse_file.R
+reservas <- get_reservas_internacionales()
+
+test_that("Returns a dataframe", {
+  expect_s3_class(reservas, "data.frame")
+})
+
+test_that("parse_file() has expected columns", {
+  expect_named(
+    reservas,
+    c("fecha", "year", "mes", "variable",  "modalidad", "value"),
+    ignore.order = TRUE
+  )
+})
+
+test_that("no dates in the future", {
+  expect_true(all(reservas$fecha <= Sys.Date()))
+})
+
+test_that("variable values are exactly 'Reservas' and 'Activos'", {
+  expect_setequal(unique(reservas$variable), c("Reservas", "Activos"))
+})
+
+test_that("modalidad values are exactly 'Brutos' and 'Netos'", {
+  expect_setequal(unique(reservas$modalidad), c("Brutos", "Netos"))
+})
+
+test_that("no missing dates or years", {
+  expect_false(anyNA(reservas$fecha))
+  expect_false(anyNA(reservas$year))
+})
+
+test_that("date day is always 1", {
+  expect_true(all(lubridate::day(reservas$fecha) == 1L))
+})
+
+test_that("values are numeric", {
+  expect_type(reservas$value, "double")
+})
