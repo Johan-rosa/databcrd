e01cd993fbbc29182cff0c32f35967c922d66de4
Johan-rosa <j.rosa@bancentral.gov.do>
2026-08-20 10:45:17 -0400

    chore: remove old exports details internal data

--- R/data.R
+++ R/data.R
@@ -12,20 +12,6 @@
 #' }
 "indicadores_bcrd_details"
 
-#' Utility complement to handle the total exports data
-#'
-#' @format ## `exports_details`
-#' A data frame with 64 rows and 6 columns:
-#' \describe{
-#'   \item{original_names}{Names in the excel file}
-#'   \item{labels}{Names with meaning and without suffix}
-#'   \item{short_names}{unique names in snake case}
-#'   \item{categoria}{Category of the indicator}
-#'   \item{nivel}{level}
-#'   \item{direct_parent}{Direct parent}
-#' }
-"exports_details"
-
 #' Utility complement to handle the total imports data
 #'
 #' @format ## `imports_details`
--- data-raw/prepar_internal_data.R
+++ data-raw/prepar_internal_data.R
@@ -1,7 +1,6 @@
 load("data/data_ipc_articulos_2020_2025.rda")
 load("data/data_ipc_articulos_long_2010_2024.rda")
 load("data/expectativas_details.rda")
-load("data/exports_details.rda")
 load("data/imports_details.rda")
 load("data/indicadores_bcrd_details.rda")
 load("data/ipc_articulos_details.rda")
@@ -13,7 +12,6 @@ usethis::use_data(
   data_ipc_articulos_long_2010_2024,
   ipc_articulos_details,
   expectativas_details,
-  exports_details,
   imports_details,
   indicadores_bcrd_details,
   detalles_indicadores_osd,
