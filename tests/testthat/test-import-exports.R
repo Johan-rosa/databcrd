3ee7852636bfd2f8cc724943dd8f0c378b60c621
Johan-rosa <j.rosa@bancentral.gov.do>
2026-08-20 10:56:24 -0400

    test: update tests

--- tests/testthat/test-import-exports.R
+++ tests/testthat/test-import-exports.R
@@ -1,13 +1,6 @@
 # Exportaciones ----
 exports_mensual <- get_exportaciones("mensual")
 
-test_that("All indicators have the same number of rows", {
-  count <- exports_mensual |>
-    dplyr::count(short_names, nivel)
-
-  testthat::expect_equal(min(count$n), max(count$n))
-})
-
 test_that("There aren't dates in the future", {
   testthat::expect_true(
     max(exports_mensual$fecha) <= lubridate::today())
@@ -15,7 +8,7 @@ test_that("There aren't dates in the future", {
 
 test_that("There aren't missing dates", {
   months_diff <- exports_mensual |>
-    dplyr::group_by(short_names, nivel) |>
+    dplyr::group_by(id, nivel) |>
     dplyr::arrange(fecha) |>
     dplyr::mutate(lag_fecha = dplyr::lag(fecha)) |>
     dplyr::filter(!is.na(lag_fecha)) |>
